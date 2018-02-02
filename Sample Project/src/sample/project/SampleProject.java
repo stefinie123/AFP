c92835701b63d7733499df7f0b8d9b2a0a501045
Stefinie fernando <minolispencer@gmail.com>
2018-02-02 11:43:37 +0530

    Second Commit

--- Sample Project/src/sample/project/SampleProject.java
+++ Sample Project/src/sample/project/SampleProject.java
@@ -17,7 +17,7 @@ public class SampleProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("I have changed here!");
+        System.out.println("I have again changed here!");
     }
     
 }
